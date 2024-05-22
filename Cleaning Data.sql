14d0e14babad9277d721bb195e6cd3e0208a4a40
Timothy Morales <167924910+timjamesmorales@users.noreply.github.com>
2024-05-22 01:04:05 -0400

    Update Cleaning Data.sql

--- Cleaning Data.sql
+++ Cleaning Data.sql
@@ -27,14 +27,14 @@ ALTER TABLE cyclistic-422905.biketrip_data.q2_2019
 RENAME COLUMN _01___Rental_Details_Local_Start_Time to start_time
 
 -- Create a new table incorporating the original data and adding 'tripduration_minutes', 'day_of_week', and 'month' columns:
-CREATE TABLE cyclistic-422905.biketrip_data.q2_2019_combined AS (
+CREATE TABLE cyclistic-422905.biketrip_data.q1_2019_combined AS (
 SELECT
  trip_id, start_time, end_time, bikeid, tripduration,
  ROUND(tripduration/60) AS tripduration_minutes,
  EXTRACT(DAYOFWEEK FROM start_time) AS day_of_week,
  EXTRACT(MONTH FROM start_time) AS month,
  from_station_id, from_station_name, to_station_id, to_station_name, usertype, gender, birthyear
-FROM `cyclistic-422905.biketrip_data.q2_2019`
+FROM `cyclistic-422905.biketrip_data.q1_2019`
 )
 
 -- Examine the minimum, maximum, and average trip duration of the new table:
@@ -42,19 +42,19 @@ SELECT
  MAX(tripduration_minutes) AS max_duration_min,
  MIN(tripduration_minutes) AS min_duration_min,
  ROUND(AVG(tripduration_minutes),2) AS avg_duration_min
-FROM `cyclistic-422905.biketrip_data.q2_2019_combined`
+FROM `cyclistic-422905.biketrip_data.q1_2019_combined`
 
 -- Check for negative trip duration values: 
 SELECT
  COUNT(*) AS negative_tripduration
-FROM `cyclistic-422905.biketrip_data.q2_2019_combined`
+FROM `cyclistic-422905.biketrip_data.q1_2019_combined`
 WHERE tripduration_minutes < 0
 -- 0 negative ride lengths reported for Q1 2019
 
 -- Check for trip duration values over 24 hrs (1440 minutes):
 SELECT
  COUNT(*) AS over_24hr
-FROM `cyclistic-422905.biketrip_data.q2_2019_combined`
+FROM `cyclistic-422905.biketrip_data.q1_2019_combined`
 WHERE tripduration_minutes > 1440
 -- 192 ride lengths over 24 hrs reported for Q1 2019
 
